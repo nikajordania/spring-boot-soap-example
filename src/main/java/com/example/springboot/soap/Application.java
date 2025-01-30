4fcd1e66e1c9691b0c30d54c7cc381693c304a45
Nika Zhordania <nika.zhordania.1@gmail.com>
2025-01-30 23:25:21 +0400

    update db config

--- src/main/java/com/example/springboot/soap/Application.java
+++ src/main/java/com/example/springboot/soap/Application.java
@@ -1,12 +1,21 @@
 package com.example.springboot.soap;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        try {
+            Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092", "-ifNotExists").start();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         SpringApplication.run(Application.class, args);
     }
 
